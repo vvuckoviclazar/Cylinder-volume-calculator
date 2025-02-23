2058420c25dec587af3163923af823fda19a0a9a
Lazar Vuckovic <lazarvuckovic777@gmail.com>
2025-02-23 08:25:37 +0100

    Finished...

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 "use strict";
+
+const radius = document.querySelector(".radius");
+const height = document.querySelector(".height");
+const form = document.querySelector("form");
+const result = document.querySelector(".result");
+
+let radiusNumber;
+let heightNumber;
+let resultNumber;
+let p = 3.14;
+
+radius.addEventListener("input", (e) => {
+  radiusNumber = e.target.value;
+});
+
+height.addEventListener("input", (e) => {
+  heightNumber = e.target.value;
+});
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  resultNumber = radiusNumber * radiusNumber * p * heightNumber;
+  result.value = resultNumber;
+});
